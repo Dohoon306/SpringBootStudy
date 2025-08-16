7b6c25e5a273e3663d5b6eae90ffb16ef4d16c14
dohoon306 <dohuni306@gmail.com>
2025-08-16 14:21:54 +0900

    6.3.4 반복 잡업을 줄여 줄 테스트 코드 작성

---
+++ src/test/java/me/dohoon/controller/BlogApiControllerTest.java
@@ -0,0 +1,38 @@
+package me.dohoon.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.dohoon.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootApplication
+@AutoConfigureMockMvc
+class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+
+}
