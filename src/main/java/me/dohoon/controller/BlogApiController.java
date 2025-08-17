c89e6bb22ce2bd22df5c6ecc47cd72f23cc3bc88
dohoon306 <dohuni306@gmail.com>
2025-08-17 16:58:31 +0900

    ~6.4.2 컨트롤러 메서드 코드 작성하기

--- src/main/java/me/dohoon/controller/BlogApiController.java
+++ src/main/java/me/dohoon/controller/BlogApiController.java
@@ -4,12 +4,16 @@ package me.dohoon.controller;
 import lombok.RequiredArgsConstructor;
 import me.dohoon.domain.Article;
 import me.dohoon.dto.AddArticleRequest;
+import me.dohoon.dto.ArticleResponse;
 import me.dohoon.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,10 +21,21 @@ public class BlogApiController {
     private final BlogService blogService;
 
     @PostMapping("/api/articles")
-    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request){//자동으로 JSON->dto변환
+        //
         Article savedArticle = blogService.save(request);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/dohoon/service/BlogService.java
+++ src/main/java/me/dohoon/service/BlogService.java
@@ -6,13 +6,20 @@ import me.dohoon.dto.AddArticleRequest;
 import me.dohoon.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
 
+    //저장 로직
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll(){
+        return blogRepository.findAll();
     }
 }
